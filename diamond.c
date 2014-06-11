68322167cfccad4fafcec50c32c6546af0fbcc9a
Jukka Nousiainen <jukka.nousiainen@ericsson.com>
2014-06-11 12:33:12 +0300

    Modified code to match the instructions

--- diamond.c
+++ diamond.c
@@ -1,19 +1,23 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_diamond(char start, int width) {
-	char list[width+1];
-	list[width] = 0;
+void print_diamond(char start) {
+    int width = (start - 'A') * 2 + 1;
+    char list[width];
 
-	for(int i = -width/2; i <= width/2; ++i) {
-		memset(list,' ', sizeof(list));
-		list[abs(i)] = start - abs(i);
-		list[(width - 1) - abs(i)] = start - abs(i);
-		printf("%s\n",list);
-	}
+    for(int i = -width/2; i <= width/2; ++i) {
+        memset(list,' ', sizeof(list));
+        list[abs(i)] = start - abs(i);
+        list[(width - 1) - abs(i)] = start - abs(i);
+        printf("%s\n",list);
+    }
 }
 
-int main(void) {
-	print_diamond('Z', 19);
-	return 0;
+int main (int argc, char const *argv[]) {
+    if(argc <= 1) {
+        print_diamond('Z');
+    }
+    print_diamond((char)argv[1][0]);
+    return 0;
 }
--- diamond.py
+++ diamond.py
@@ -1,9 +1,10 @@
-def main(start, width):
-    for i in range (-(width/2),width/2+1):
-        list = [' ' for j in range (-(width/2),width/2+1)]
+def main(start):
+    width = (ord(start) - ord('A')) * 2 + 1
+    for i in range (-(width/2), width/2 + 1):
+        list = [' ' for j in range (-(width/2),width/2 + 1)]
         list[abs(i)] = chr(ord(start) - abs(i))
-        list[-abs(i)-1] = chr(ord(start) - abs(i))
+        list[-abs(i) - 1] = chr(ord(start) - abs(i))
         print "".join(list) 
         
 if __name__ == '__main__':
-    main('Z', 20)
+    main('Z')
